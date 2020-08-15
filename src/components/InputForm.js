f5539a2e79e2d31daadef9b9e7975ef5e70b7cf3
Michael P Nance <michael.p.nance@gmail.com>
2020-08-15 14:27:16 -0400

    Added state and methods props to InputForm component

--- src/components/InputForm.js
+++ src/components/InputForm.js
@@ -1,21 +1,28 @@
 import React, { useState } from "react";
 
 // need to add local and global state to this one
-export default () => {
+export default ({ state, methods }) => {
+    // return (
+    //     <div className="field has-addons">
+    //         <div className="control is-expanded">
+    //             <input
+    //                 className="input"
+    //                 type="text"
+    //                 placeholder="Ingredients"
+    //             />
+    //         </div>
+    //         <div className="control">
+    //             <a href="/search" className="button is-warning">
+    //                 Add
+    //             </a>
+    //         </div>
+    //     </div>
+    // );
+    const onSubmitHandler = methods ? methods.onSubmitHandler : () => {};
     return (
-        <div className="field has-addons">
-            <div className="control is-expanded">
-                <input
-                    className="input"
-                    type="text"
-                    placeholder="Ingredients"
-                />
-            </div>
-            <div className="control">
-                <a href="/search" className="button is-warning">
-                    Add
-                </a>
-            </div>
-        </div>
+        <form className="field has-addons" onSubmit={onSubmitHandler}>
+            <input className="input" type="text" placeholder="Ingredients" />
+            <button className="button is-warning">Add</button>
+        </form>
     );
 };
