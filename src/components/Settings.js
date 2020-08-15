41de90406cf42340a958c0ed430ec145d8659a23
Michael P Nance <michael.p.nance@gmail.com>
2020-08-15 16:53:01 -0400

    Fixed formatting for Settings component

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -6,27 +6,29 @@ import Pantry from "./Pantry";
 import { intolerances, diets, cuisines } from "../assets/js/Lists";
 import { v4 as uuid } from "uuid";
 
-const Settings = (props) => (
+const Settings = ({ state }) => (
     <>
         <label className="label has-text-centered">Food Intolerances</label>
-        <FlexContainer> 
-            children={intolerances.map((item) => (
+        <FlexContainer>
+            {intolerances.map((item) => (
                 <CheckBox key={uuid()} text={item} />
             ))}
-        </FlexContainer>  <FlexContainer> 
+        </FlexContainer>{" "}
         <label className="label has-text-centered">Diets</label>
-            children={diets.map((item) => (
+        <FlexContainer>
+            {diets.map((item) => (
                 <CheckBox key={uuid()} text={item} />
             ))}
-    </FlexContainer> <FlexContainer> 
-    <label className="label has-text-centered">Excluded Cuisines</label>
-            children={cuisines.map((item) => (
+        </FlexContainer>{" "}
+        <label className="label has-text-centered">Excluded Cuisines</label>
+        <FlexContainer>
+            {cuisines.map((item) => (
                 <CheckBox key={uuid()} text={item} />
             ))}
+        </FlexContainer>
         <label className="label has-text-centered">Excluded Ingredients</label>
         <InputForm placeholder="Ingredients" />
         <Pantry items={["meekrob", "sun dried tomatos"]} />
-    </FlexContainer>
     </>
 );
 
