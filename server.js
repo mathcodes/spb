8d05bcc91eab6c34bfe023de74ad1c32df04d7df
Michael P Nance <michael.p.nance@gmail.com>
2020-08-15 23:01:45 -0400

    Removed unused files. Added spoonacular API route to server and update package.json with required dependencies.

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 
 const mongoose = require("mongoose");
-const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -10,15 +9,32 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+    app.use(express.static("client/build"));
 }
-// Add routes, both API and view
-app.use(routes);
+
+// Routes
+require("dotenv").config();
+const fetch = require("node-fetch");
+const url = "https://api.spoonacular.com/recipes/complexSearch";
+const getRecipes = (params) => {
+    let url_ = new URL(url);
+    Object.keys(params).forEach((key) =>
+        url_.searchParams.append(key, params[key])
+    );
+    return fetch(url_).then((response) => response.json());
+};
+app.post("/api/search", (req, res) => {
+    getRecipes({ ...req.body, apiKey: process.env.apiKey }).then((recipes) =>
+        res.json(recipes)
+    );
+});
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
+mongoose.connect(
+    process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist"
+);
 
 // Start the API server
-app.listen(PORT, function() {
-  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+app.listen(PORT, function () {
+    console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
 });
