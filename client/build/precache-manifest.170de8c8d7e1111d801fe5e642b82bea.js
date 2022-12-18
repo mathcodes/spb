1845b81323d2bfc9b63d552086b8fa91ee1cb9a4
mathcodes <jonpchristie@gmail.com>
2022-12-18 02:08:03 -0500

    redeploying to vercel

---
+++ client/build/precache-manifest.170de8c8d7e1111d801fe5e642b82bea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34d34882b3a92149f0ee1e21c41f376f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3fb2d092d7f7a049cee1",
+    "url": "/static/css/main.c327ca4d.chunk.css"
+  },
+  {
+    "revision": "ceacc29b6af395778891",
+    "url": "/static/js/2.0c9f44e8.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/static/js/2.0c9f44e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3fb2d092d7f7a049cee1",
+    "url": "/static/js/main.3234f488.chunk.js"
+  },
+  {
+    "revision": "00fedbb296154872a916",
+    "url": "/static/js/runtime-main.1ab6aedf.js"
+  },
+  {
+    "revision": "b0d88454525308bf5b6ea3202cf10300",
+    "url": "/static/media/SPBRetroSlantAcross1382x120.b0d88454.png"
+  }
+]);
